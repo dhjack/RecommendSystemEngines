8e8927252fbf10bf2fd85ad55d3313e40b3ca37b
xiongwen <dhjack88@gmail.com>
2017-08-09 10:47:55 +0800

    lfs for sql data

---
+++ data/t.movies.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:10470446a60d18791fc755d8b3bd06cd94b907bafdacc8ca1c0d53a20fd34219
+size 58594118
---
+++ data/t.rating.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c884dfbf2cc37bd80b1575a5dd4375d13884115e5ce61ed48586ca2ddaf74238
+size 1868099
